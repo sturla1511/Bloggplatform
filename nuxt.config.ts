79ddd926cd460e25f6f0216b1f36cd7f070f78d1
FluffyFire <62662486+sturla1511@users.noreply.github.com>
2024-04-18 15:52:20 +0200

    added scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  ssr: false,
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: "/api/graphql",
+      },
+    },
+  },
+  css: ["~/assets/css/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "./assets/css/variables.scss";',
+        },
+      },
+    },
+    server: {
+      fs: {
+        strict: false,
+      },
+    },
+  },
+});
